1a01f5503804e04be0d8401c966969f4c2fec47b
弘树 <tiehang.lth@alibaba-inc.com>
2014-08-08 18:10:43 +0800

    sync daily version 2.0.3

---
+++ 2.0.3/build/mods-min.js
@@ -0,0 +1,2 @@
+/*!build time : 2014-08-08 6:10:14 PM*/
+KISSY.config("modules",{"kg/textbox/2.0.1/text":{requires:["dom","node","base","json","overlay"]}});
---
+++ 2.0.3/build/mods.js
@@ -0,0 +1,3 @@
+KISSY.config('modules', {
+ 'kg/textbox/2.0.1/text': { requires: ['dom', 'node', 'base', 'json', 'overlay']} 
+});
